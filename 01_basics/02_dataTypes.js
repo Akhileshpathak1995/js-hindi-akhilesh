dbbf2c6fc4e2b910f4961e5d3e09e90910df24b2
Akhileshpathak1995 <akhileshkumarpathak00@gmail.com>
2025-06-25 06:11:35 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"  //treat all js code as newer version
+
+//alert(3 + 3)   //it will not work here beacuse we are using nodejs, not browser
+
+console.log("Akhilesh Pathak");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => still value is not assigned
+//Symbol => unique
+
+//object
+console.log(typeof undefined);   //undefined
+console.log(typeof null);     //object
